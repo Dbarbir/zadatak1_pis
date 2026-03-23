660277b118df593a4ffd6ef8b7d7125aa1c0a842
dev1ce2004 <ktesij00@fesb.hr>
2026-03-23 19:50:31 +0100

    Ovo je novododana klasa

---
+++ Class1.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace zadatak1_pis
+{
+    public class Klasa
+    {
+        public string poruka = "Ovo je klasa";
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Klasa k = new Klasa();
+            Console.WriteLine(k.poruka);
+        }
+    }
+}
